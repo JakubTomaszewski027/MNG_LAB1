3b7f95b3077a31db43d75e3322e84415a0b2f37e
Jakub Tomaszewski <jakubto2001@gmail.com>
2023-04-11 12:13:22 +0200

    All functions

--- mail.py
+++ mail.py
@@ -2,23 +2,40 @@ import re
 
 class EmailExtractor:
 
-    def __init__(self, emailD: str):
+    def __init__(self, emailD):
         self.email = emailD
         self.regex = r"(?P<name>[a-z]+)\.(?P<surname>[a-z]+)[0-9]*@(?P<is_student>(student\.)?)(wat\.edu\.pl)"
 
-    def is_student(self) -> bool:
-        x = re.compile("")
-        x.match(self.email)
-        return False
-
-    def is_male(self) -> bool:
-        return False
-
     def get_name(self) -> str:
-        return "Norbert"
+        x = re.compile(self.regex)
+        y = x.findall(self.email)
+        name = y[0][1]
+        return name.capitalize()
 
     def get_surname(self) -> str:
-        return "Waszkowiak"
+        x = re.compile(self.regex)
+        y = x.findall(self.email)
+        surname = y[0][1]
+        return surname.capitalize()
+
+    def is_student(self) -> bool:
+        x = re.compile(self.regex)
+        y = x.findall(self.email)
+        if y[0][1] == "":
+            return False
+        else:
+            return True
+
+    def is_male(self) -> bool:
+        name = self.get_name()
+        x = name[-1]
+        if x == 'a':
+            return False
+        else:
+            return True
+
+
+
 
 
 
